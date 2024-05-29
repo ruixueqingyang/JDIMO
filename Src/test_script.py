11a952e6f668becbbe90349aa8fa2f1d0f558863
瑞雪轻飏 <ruixueqingyang@163.com>
2024-05-30 01:00:40 +0800

    update

--- Src/test_script.py
+++ Src/test_script.py
@@ -2,6 +2,7 @@
 import sys, os, math, time, copy
 import numpy as np
 from test_profiling import ETOpt
+from JDIMO import JDIMO
 
 algorithm = "subgraph"
 
@@ -20,5 +21,24 @@ ETOpt(model_name, algorithm)
 # model_name = "yolov4"
 # ETOpt(model_name, algorithm)
 
+# vgg16 相关路径
+model_folder_dir = os.path.join("home", "user_name", "onnx_model_zoo", "vgg16")
+model_file_name = "vgg16.onnx"
+model_file_dir = os.path.join(model_folder_dir, model_file_name)
+BatchSize = 1
+listUnknownDims = []
+listInputName = ["data"]
+listInputShape = [[1,3,224,224]]
+listRange = [[0, 255]]
+useless_prefix = "vgg0_"
+
+dictInputTensor = {}
+dictInputTensor["data"] = np.random.random([1,3,224,224]).astype(np.float32) * 255
+listInputTensor = [dictInputTensor["data"]]
+listInputShape = [[1,3,224,224]]
+listRange = [[0, 255]]
+useless_prefix = "vgg0_"
+
+JDIMO(model_file_dir, BatchSize, listUnknownDims, listRange, useless_prefix)
 
 
